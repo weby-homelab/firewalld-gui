3198ef3227b04fbd1e94ccf050aeb3e15ef1ec63
Weby Homelab <homelab@weby.srvrs.top>
2026-03-19 00:27:13 +0200

    fix: add niftywall to allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["firewalld-gui.srvrs.top"],
+    allowedHosts: ["firewalld-gui.srvrs.top", "niftywall.srvrs.top"],
     host: true,
     port: 5173
   }
