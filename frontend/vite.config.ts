459cada25338c786651c9fd4d7a30fb615f3d798
weby-homelab <rekvizitor.ua@gmail.com>
2026-05-09 17:19:01 +0300

    feat: implement dynamic versioning in UI from package.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
   server: {
     allowedHosts: ["firewalld-gui.srvrs.top", "niftywall.srvrs.top"],
     host: true,
